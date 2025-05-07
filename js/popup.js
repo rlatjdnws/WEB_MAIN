c7085b2da98ca09bccfb7fb3bd3956ef53f1f29f
admin <ksj041223@gmail.com>
2025-05-07 10:40:03 +0900

    10주차 실습

--- js/popup.js
+++ js/popup.js
@@ -33,26 +33,6 @@ function closePopup() {
         self.close();
     }
 }
-    
-
-// var close_time; // 시간 정보
-// var close_time2 = 10; // 10초 설정
-// clearTimeout(close_time); // 재호출 정지
-// close_time= setTimeout("close_window()", 10000);
-// // 1/1000 초 지정, 바로 시작
-// show_time(); // 실시간 시간 보여주기
-
-// function show_time(){
-//     let divClock = document.getElementById('Time');
-//     divClock.innerText = close_time2; // 10초 삽입 시작
-//     close_time2--; // 1초씩 감소
-//     setTimeout(show_time, 1000); //1초마다 갱신
-// }
-
-// function close_window() { // 함수 정의
-//     window.close(); // 윈도우 닫기X
-// }
-
 
 function show_clock(){
     let currentDate = new Date(); // 현재 시스템 날짜 객체 생성
@@ -78,13 +58,6 @@ function show_clock(){
 
     setTimeout(show_clock, 1000); //1초마다 갱신
 }
-
-// function over(obj) {
-//     obj.src="images/logo2.png";
-// }
-// function out(obj) {
-//     obj.src="images/LOGO.png";
-// } //로고 이미지 변경
 
 const over = (obj) => {
     obj.src = "images/logo2.png";
